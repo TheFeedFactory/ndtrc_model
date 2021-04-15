8646f410442cbed04629ebd369f84d10a2442981
Maurice Jumelet <maurice@jumelet.net>
2021-04-15 11:28:58 +0200

    added errorMessage to fetched

--- src/main/groovy/nl/ithelden/model/FetchedEntry.groovy
+++ src/main/groovy/nl/ithelden/model/FetchedEntry.groovy
@@ -1,31 +1,34 @@
 package nl.ithelden.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
+
 import groovy.transform.ToString
 import org.joda.time.DateTime
 
 @ToString
 class FetchedEntry {
-    @JsonProperty String feed
-    @JsonProperty String sourceId
-    @JsonProperty String label
-    @JsonProperty DateTime created // http://purl.org/dc/terms/created
-    @JsonProperty DateTime modified // http://purl.org/dc/terms/modified
+    String feed
+    String sourceId
+    String sourceUrl
+    String errorMessage
 
-    @JsonProperty String externalId
-    @JsonProperty Map<String, Object> data
-    @JsonProperty ExternalLocationInfo externalLocationInfo
+    String label
+    DateTime created // http://purl.org/dc/terms/created
+    DateTime modified // http://purl.org/dc/terms/modified
+
+    String externalId
+    Map<String, Object> data
+    ExternalLocationInfo externalLocationInfo
 
     @ToString
     public static class ExternalLocationInfo {
-        @JsonProperty String externalId // location id used in feeds
-        @JsonProperty String locationName //name of location in original feed
-        @JsonProperty String address
-        @JsonProperty String housenr
-        @JsonProperty String zipcode
-        @JsonProperty String city
-        @JsonProperty String latitude
-        @JsonProperty String longitude
-        @JsonProperty String trcid // trcid as known in the TRC
+        String externalId // location id used in feeds
+        String locationName //name of location in original feed
+        String address
+        String housenr
+        String zipcode
+        String city
+        String latitude
+        String longitude
+        String trcid // trcid as known in the TRC
     }
 }
