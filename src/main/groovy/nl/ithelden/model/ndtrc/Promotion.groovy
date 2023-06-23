034656ba058f845dde4ac54ae5771324d89b67f2
Maurice Jumelet <m.jumelet@dutchview.com>
2023-06-23 10:37:14 +0200

    refactored promotions

--- src/main/groovy/nl/ithelden/model/ndtrc/Promotion.groovy
+++ src/main/groovy/nl/ithelden/model/ndtrc/Promotion.groovy
@@ -6,14 +6,14 @@ import org.joda.time.DateTime
 
 @ToString(includeNames = true)
 class Promotion {
+    List<String> products
     PromotionType promotionType
     Discount discount
     List<PromotionTranslation> translations
-    List<PromotionDetailsUrl> detailsUrls
+    List<Contactinfo.Url> detailsUrls
     DateTime startDate
     DateTime endDate
     boolean enabled = true
-    List<PromotionTargetAudience> targetAudiences
     List<Calendar.PatternDate.Open> opens
 
     @ToString(includeNames = true)
@@ -28,11 +28,5 @@ class Promotion {
         String lang, description
     }
 
-    @ToString(includeNames = true)
-    static class PromotionDetailsUrl {
-        String lang, url
-    }
-
     static enum PromotionType { free, discount, gift, allowance }
-    static enum PromotionTargetAudience { residents, visitors }
 }
