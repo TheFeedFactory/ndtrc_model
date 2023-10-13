51fd1fd6d3a1888ad39f0f55ee4400c810f36b92
Maurice Jumelet <m.jumelet@dutchview.com>
2023-10-13 10:32:51 +0200

    added File normalise youtube

--- src/main/groovy/nl/ithelden/model/ndtrc/File.groovy
+++ src/main/groovy/nl/ithelden/model/ndtrc/File.groovy
@@ -42,7 +42,45 @@ class File {
                 mediatype = MediaType.photo
             }
         }
+
+        // if the filetype is video convert any url to the standard youtube format
+        // al link the the format of
+        if (hlink && filetype == FileType.youtube) {
+            hlink = normalizeYouTubeURL(hlink)
+        }
     }
+
     enum FileType { jpeg, jpg, gif, png, mp3, pdf, gpx, kml, youtube, kmz, vimeo, tif, bmp, jfif, tiff, webp }
     enum MediaType { poster, other, audio, brochure, photo, logo, video, roadmap, text, attachment, qr }
+
+    static def normalizeYouTubeURL(String url) {
+        if (!url?.trim()) return url
+
+        // Regular expression patterns for different YouTube URL formats
+        def standardPattern = ~/https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/
+        def shortenedPattern = ~/https:\/\/youtu\.be\/([a-zA-Z0-9_-]+)/
+        def embedPattern = ~/https:\/\/www\.youtube\.com\/embed\/([a-zA-Z0-9_-]+)/
+
+        def matcher = standardPattern.matcher(url)
+
+        // Check if it's already in the desired format
+        if (matcher.find()) {
+            return "https://www.youtube.com/watch?v=${matcher.group(1)}"
+        }
+
+        // Check if it's in the shortened format
+        matcher = shortenedPattern.matcher(url)
+        if (matcher.find()) {
+            return "https://www.youtube.com/watch?v=${matcher.group(1)}"
+        }
+
+        // Check if it's in the embed format
+        matcher = embedPattern.matcher(url)
+        if (matcher.find()) {
+            return "https://www.youtube.com/watch?v=${matcher.group(1)}"
+        }
+
+        // If none of the patterns match, return the original URL (or you could return null or throw an exception if you prefer)
+        return url
+    }
 }
---
+++ src/test/groovy/nl/ithelden/FileTest.groovy
@@ -0,0 +1,24 @@
+package nl.ithelden
+
+import nl.ithelden.model.ndtrc.File
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class FileTest {
+
+    @Test
+    void testYouTube() {
+        String url1 = "https://www.youtube.com/watch?v=hVlT0PN3y-k"
+        String n1 = File.normalizeYouTubeURL(url1)
+
+        String url2 = "https://youtu.be/hVlT0PN3y-k"
+        String n2 = File.normalizeYouTubeURL(url2)
+
+        String url3 = "https://www.youtube.com/embed/hVlT0PN3y-k"
+        String n3 = File.normalizeYouTubeURL(url3)
+
+        Assertions.assertEquals("https://www.youtube.com/watch?v=hVlT0PN3y-k", n1)
+        Assertions.assertEquals("https://www.youtube.com/watch?v=hVlT0PN3y-k", n2)
+        Assertions.assertEquals("https://www.youtube.com/watch?v=hVlT0PN3y-k", n3)
+    }
+}
