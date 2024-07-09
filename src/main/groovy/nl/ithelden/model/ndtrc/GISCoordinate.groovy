71dd0069d233343caf206d4f5a57f137adc35119
Maurice Jumelet <m.jumelet@dutchview.com>
2024-07-09 13:25:56 +0200

    less JSON cluttor

--- src/main/groovy/nl/ithelden/model/ndtrc/GISCoordinate.groovy
+++ src/main/groovy/nl/ithelden/model/ndtrc/GISCoordinate.groovy
@@ -1,11 +1,13 @@
 package nl.ithelden.model.ndtrc
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import groovy.transform.ToString
 import nl.ithelden.model.util.StringUtils
 
 @ToString(includeNames = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class GISCoordinate {
     @JsonProperty String xcoordinate
     @JsonProperty String ycoordinate
