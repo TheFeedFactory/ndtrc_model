8b4ed412b94149a347e5b4895f7762f7fb10afa9
Maurice Jumelet <m.jumelet@dutchview.com>
2025-01-10 15:33:31 +0100

    cleanup

--- src/main/groovy/nl/ithelden/model/ndtrc/SubItemGroup.groovy
+++ src/main/groovy/nl/ithelden/model/ndtrc/SubItemGroup.groovy
@@ -1,7 +1,6 @@
 package nl.ithelden.model.ndtrc
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonProperty
 import groovy.transform.ToString
 
 /**
@@ -10,31 +9,15 @@ import groovy.transform.ToString
 @ToString(includeNames = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 class SubItemGroup {
-    @JsonProperty String trcid // ID of this type (random)
-    @JsonProperty TRCItemCategories.Type type
-    @JsonProperty List<TRCItemCategories.Category> categories = []  // Categories/properties of the item
-    @JsonProperty List<SubItemTranslation> subItemTranslations
-    @JsonProperty List<File> media // media
+    String trcid // ID of this type (random)
+    TRCItemCategories.Type type
+    List<TRCItemCategories.Category> categories = []  // Categories/properties of the item
+    List<SubItemTranslation> subItemTranslations
+    List<File> media // media
 
     @ToString(includeNames = true)
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     static class SubItemTranslation {
         String lang, title
-    }
-
-    void cleanEmptyItems() {
-        this.categories = this.categories?.findAll { TRCItemCategories.Category category ->
-            if ("freetext".equalsIgnoreCase(category.datatype) && !category.value) {
-                return false
-            }
-            if ("multichoice".equalsIgnoreCase(category.datatype) && !category.valueid) {
-                return false
-            }
-            if ("choice".equalsIgnoreCase(category.datatype) && !category.valueid) {
-                return false
-            }
-
-            return true
-        }
     }
 }
